35a32c9c39f31268ac20732e2ead3243cb04270b
ziadd1 <ziadtt5@gmail.com>
2024-08-04 13:57:50 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MyAppState extends State<MyApp> {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
             fontFamily: 'Brodien',
-            
+
             scaffoldBackgroundColor: const Color.fromRGBO(248, 250, 255, 1)),
       ),
     );
