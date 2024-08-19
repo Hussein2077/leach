46d9bf23181487d9e009756727cd609427735c32
Hussein Yasser <119596187+Hussein2077@users.noreply.github.com>
2024-08-19 16:34:04 +0300

    .

--- lib/features/profile/presentation/profile/componant/booking_history.dart
+++ lib/features/profile/presentation/profile/componant/booking_history.dart
@@ -1,14 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:leach/core/resource_manager/asset_path.dart';
-import 'package:leach/core/resource_manager/colors.dart';
 import 'package:leach/core/resource_manager/string_manager.dart';
 import 'package:leach/core/utils/app_size.dart';
 import 'package:leach/core/widgets/cutom_text.dart';
 import 'package:leach/core/widgets/leading_icon.dart';
-import 'package:leach/core/widgets/main_button.dart';
-import 'package:leach/features/auth/presentation/widgets/leading_with_icon.dart';
-import 'package:leach/features/home/widgets/doctor_card.dart';
 import 'package:leach/features/profile/presentation/profile/widgets/doctor_card2.dart';
 
 class BookingHistory extends StatelessWidget {
@@ -17,7 +13,7 @@ class BookingHistory extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(68,82, 255, 1),
+      backgroundColor: const Color.fromRGBO(68,82, 255, 1),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.only(
